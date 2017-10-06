7e5b84b389d3a86a53bb63b000608db26b2b7779
milind Panwala <milind2309@gmail.com>
2017-10-06 14:40:51 -0500

    Add project files.

---
+++ GroceryList/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GroceryList.Startup))]
+namespace GroceryList
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
